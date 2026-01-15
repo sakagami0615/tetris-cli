88a5a5334d1aa99f7ee352aa7eb30d284ff7d1b3
sakagami0615 <sakagami0615@gmail.com>
2026-01-15 22:26:25 +0900

    ゲーム中のキー入力をターミナル非表示に設定

--- tetris_cli/src/common/console.py
+++ tetris_cli/src/common/console.py
@@ -25,3 +25,51 @@ def clear_input_buffer() -> None:
     else:
         import termios
         termios.tcflush(sys.stdin, termios.TCIOFLUSH)
+
+
+def disable_echo():
+    """ターミナルのエコーを無効化（キー入力を画面に表示しない）"""
+    if platform.system() == 'Windows':
+        try:
+            import msvcrt
+            # Windowsではコンソールモードを変更
+            import ctypes
+            kernel32 = ctypes.windll.kernel32
+            handle = kernel32.GetStdHandle(-10)  # STD_INPUT_HANDLE
+            mode = ctypes.c_ulong()
+            kernel32.GetConsoleMode(handle, ctypes.byref(mode))
+            # ENABLE_ECHO_INPUT (0x0004) を無効化
+            new_mode = mode.value & ~0x0004
+            kernel32.SetConsoleMode(handle, new_mode)
+            return mode.value
+        except Exception:
+            return None
+    else:
+        import tty
+        import termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        new_settings = termios.tcgetattr(fd)
+        # ECHO フラグを無効化
+        new_settings[3] = new_settings[3] & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSADRAIN, new_settings)
+        return old_settings
+
+
+def restore_echo(old_settings):
+    """ターミナルのエコーを元に戻す"""
+    if old_settings is None:
+        return
+
+    if platform.system() == 'Windows':
+        try:
+            import ctypes
+            kernel32 = ctypes.windll.kernel32
+            handle = kernel32.GetStdHandle(-10)
+            kernel32.SetConsoleMode(handle, old_settings)
+        except Exception:
+            pass
+    else:
+        import termios
+        fd = sys.stdin.fileno()
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
--- tetris_cli/src/game_manager.py
+++ tetris_cli/src/game_manager.py
@@ -10,16 +10,21 @@ from tetris_cli.src.tetris.const import (
 
 class GameManager:
     """ゲーム全体を管理するマネージャークラス"""
-    game: Game
-    game_loop_trigger: TimeTrigger
+    _game: Game
+    _game_loop_trigger: TimeTrigger
+    _old_terminal_settings = None
 
     def __init__(self):
-        self.game = Game()
-        self.game_loop_trigger = TimeTrigger(interval=GAME_LOOP_TRIGGER_TIME)
+        self._game = Game()
+        self._game_loop_trigger = TimeTrigger(interval=GAME_LOOP_TRIGGER_TIME)
+        # ターミナルのエコーを無効化（キー入力を画面に表示しない）
+        self._old_terminal_settings = console.disable_echo()
 
     def __del__(self):
         # 最終描画
-        self.game.draw(is_cursor_up=False)
+        self._game.draw(is_cursor_up=False)
+        # ターミナル設定を元に戻す
+        console.restore_echo(self._old_terminal_settings)
 
     def _is_quit(self) -> bool:
         """終了キーが押されたかチェック"""
@@ -29,10 +34,10 @@ class GameManager:
     def game_loop(self) -> None:
         """ゲームループを実行"""
         try:
-            while self.game.is_continue:
-                if self.game_loop_trigger.is_trigger():
-                    self.game.update()
-                    self.game.draw()
+            while self._game.is_continue:
+                if self._game_loop_trigger.is_trigger():
+                    self._game.update()
+                    self._game.draw()
                     KeyManager(HOT_KEY_LIST).update()
 
                 if self._is_quit():
@@ -40,6 +45,8 @@ class GameManager:
         finally:
             KeyManager(HOT_KEY_LIST).stop()
             console.clear_input_buffer()
+            # ターミナル設定を元に戻す
+            console.restore_echo(self._old_terminal_settings)
 
 
 def run():
